4f783830d4d1272bdd58bac4696ef56879a30a18
Siddharth Betala <62656543+sid-betalol@users.noreply.github.com>
2025-06-05 17:55:19 +0530

    fix: add code for loading environments in example notebooks

--- src/llm_synthesis/__init__.py
+++ src/llm_synthesis/__init__.py
@@ -1,1 +1,16 @@
 # TODO: add public API here -- fucntions to be accessible to public
+
+# Public API exports
+from llm_synthesis.utils import (
+    extract_markdown,
+    process_paper_with_figure_descriptions,
+    EnhancedMarkdownProcessor,
+    configure_dspy,
+)
+
+__all__ = [
+    "extract_markdown",
+    "process_paper_with_figure_descriptions",
+    "EnhancedMarkdownProcessor",
+    "configure_dspy",
+]
