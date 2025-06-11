3427a1766f43b63840c224b675c1369565e1f6d9
Magdalena Lederbauer <magdalena.lederbauer@gmail.com>
2025-06-11 17:58:00 +0200

    fix: cleanup repo, remove unneeded functions

--- src/llm_synthesis/__init__.py
+++ src/llm_synthesis/__init__.py
@@ -2,15 +2,9 @@
 
 # Public API exports
 from llm_synthesis.utils import (
-    extract_markdown,
-    process_paper_with_figure_descriptions,
-    EnhancedMarkdownProcessor,
     configure_dspy,
 )
 
 __all__ = [
-    "extract_markdown",
-    "process_paper_with_figure_descriptions",
-    "EnhancedMarkdownProcessor",
     "configure_dspy",
 ]
--- src/llm_synthesis/transformers/base.py
+++ src/llm_synthesis/transformers/base.py
@@ -1,16 +1,14 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
-
-import dspy
 
 T = TypeVar("T")
 R = TypeVar("R")
 
 
-class ExtractorInterface(dspy.Module, Generic[T, R], metaclass=ABCMeta):
+class ExtractorInterface(ABC, Generic[T, R]):
     """
-    Generic interface for an extractor that takes an input of type T and returns
-    an output of type R.
+    Generic interface for an extractor that takes an input
+    of type T and returns an output of type R.
     """
 
     @abstractmethod
