4507b5745216750fdae1737f66169c8046139b2a
Siddharth Betala <62656543+sid-betalol@users.noreply.github.com>
2025-07-16 14:58:57 +0530

    [Evaluation]feat: LLM-as-a-judge for new ontology (#81)
    * Initial empty commit
    
    * feat: LLM-as-a-judge for extended ontology
    
    * feat: llm a s a judge for extended ontology
    
    * fix: default other configs to null to not crowd config
    
    * refactor: change the test script ot a notebook to showcae functionality
    
    ---------
    
    Co-authored-by: mlederbauer <magdalena.lederbauer@gmail.com>

--- src/llm_synthesis/transformers/figure_extraction/__init__.py
+++ src/llm_synthesis/transformers/figure_extraction/__init__.py
@@ -1,6 +1,6 @@
-from llm_synthesis.transformers.figure_extraction.regex_figure_extractor import (
-    FigureExtractorMarkdown,
-)
+from llm_synthesis.transformers.figure_extraction import regex_figure_extractor
+
+FigureExtractorMarkdown = regex_figure_extractor.FigureExtractorMarkdown
 
 __all__ = [
     "FigureExtractorMarkdown",
--- src/llm_synthesis/transformers/synthesis_extraction/__init__.py
+++ src/llm_synthesis/transformers/synthesis_extraction/__init__.py
@@ -1,6 +1,12 @@
-from llm_synthesis.transformers.synthesis_extraction.dspy_synthesis_extraction import (
-    DspyStructuredSynthesisExtractor,
-    make_dspy_structured_synthesis_extractor_signature,
+from llm_synthesis.transformers.synthesis_extraction import (
+    dspy_synthesis_extraction,
+)
+
+DspyStructuredSynthesisExtractor = (
+    dspy_synthesis_extraction.DspyStructuredSynthesisExtractor
+)
+make_dspy_structured_synthesis_extractor_signature = (
+    dspy_synthesis_extraction.make_dspy_structured_synthesis_extractor_signature
 )
 
 __all__ = [
