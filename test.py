a74b60c04c99b131e54e037717b91380d9f7e53c
Xiyao <xiyao.li@entalpic.ai>
2025-07-17 10:32:51 +0200

    refactor: delete redundant files

--- test.py
+++
@@ -1,9 +0,0 @@
-from llm_synthesis.metrics.figure_extraction.generate_scatterplots import (
-    generate_synthetic_plots,
-)
-
-num_plots = 5
-images_path = "./data/figure_output/"
-groundtruths_path = "./data/coor_output/"
-
-generate_synthetic_plots(num_plots, images_path, groundtruths_path)
