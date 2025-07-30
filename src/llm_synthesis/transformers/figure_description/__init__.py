f4c72a54d3f1f8ea4aca4ca68ffdcb9c720f430a
mlederbauer <magdalena.lederbauer@gmail.com>
2025-07-30 10:08:30 +0200

    refactor: remove figure description extractor

--- src/llm_synthesis/transformers/figure_description/__init__.py
+++
@@ -1,9 +0,0 @@
-from llm_synthesis.transformers.figure_description.dspy_figure_description import (
-    DspyFigureDescriptionExtractor,
-    make_dspy_figure_description_extractor_signature,
-)
-
-__all__ = [
-    "DspyFigureDescriptionExtractor",
-    "make_dspy_figure_description_extractor_signature",
-]
--- src/llm_synthesis/transformers/figure_description/base.py
+++
@@ -1,6 +0,0 @@
-from llm_synthesis.models.figure import FigureInfoWithPaper
-from llm_synthesis.transformers.base import ExtractorInterface
-
-FigureDescriptionExtractorInterface = ExtractorInterface[
-    FigureInfoWithPaper, str
-]
