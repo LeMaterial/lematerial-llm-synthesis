9f512234ba1291ecb95027cb2c9f3c0194090200
mlederbauer <magdalena.lederbauer@gmail.com>
2025-08-01 11:19:54 +0200

    feat: add markers

--- src/llm_synthesis/utils/visualization.py
+++ src/llm_synthesis/utils/visualization.py
@@ -1,15 +1,34 @@
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
 
 from llm_synthesis.models.plot import ExtractedLinePlotData
 
 
 def visualize_line_chart(data: ExtractedLinePlotData):
     names = list(data.name_to_coordinates.keys())
-    for name in names:
+
+    markers = [
+        "o",
+        "^",
+        "s",
+        "D",
+        "v",
+        "<",
+        ">",
+        "p",
+        "*",
+        "h",
+        "H",
+        "+",
+        "x",
+        "|",
+        "_",
+    ]
+
+    for i, name in enumerate(names):
         coords = data.name_to_coordinates[name]
         x, y = zip(*coords)
-        plt.plot(x, y, label=name)
+        marker = markers[i % len(markers)]
+        plt.plot(x, y, label=name, marker=marker)
 
     xlabel = f"{data.x_axis_label}_({data.x_axis_unit})"
     ylabel = f"{data.y_left_axis_label}_({data.y_left_axis_unit})"
